cc11a4b2b84c850fd8afbbdf94e7d638eddd77bd
Piet Helzel <piet.helzel@outlook.com>
2021-11-06 23:10:36 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod interface;
+use interface::Interface;
+
+mod window_manager;
+
+use glium::glutin::event_loop::EventLoop;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::with_user_event();
+    let interface = Interface::new(event_loop);
+
+    interface.run();
 }
