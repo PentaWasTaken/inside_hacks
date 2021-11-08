0efe395d4fac31eef07650920956043760bebb53
Piet Helzel <piet.helzel@outlook.com>
2021-11-08 22:03:45 +0100

    Finished functions to update size and position

--- src/window_manager.rs
+++ src/window_manager.rs
@@ -1,14 +1,16 @@
 use glium::Display;
 
-use glutin::dpi::{PhysicalPosition, PhysicalSize};
-
-use std::mem::{size_of, transmute};
 use std::ffi::CString;
+use std::mem::{size_of, transmute};
 use std::ptr;
 
-use windows::Win32::Foundation::{HWND, RECT, PSTR};
+use windows::Win32::Foundation::{HWND, PSTR, RECT};
 use windows::Win32::Graphics::Dwm::{DwmGetWindowAttribute, DWMWINDOWATTRIBUTE};
-use windows::Win32::UI::WindowsAndMessaging::FindWindowA;
+use windows::Win32::UI::WindowsAndMessaging::{FindWindowA, GetSystemMetrics, SYSTEM_METRICS_INDEX};
+
+use glutin::dpi::{PhysicalPosition, PhysicalSize};
+
+use egui::Rect;
 
 pub struct WindowManager {
     game_window: HWND,
@@ -21,16 +23,15 @@ impl WindowManager {
 
         let a = PSTR(ptr::null_mut());
 
-        let hwnd = unsafe { FindWindowA(a, win_name)};
+        let game_window = unsafe { FindWindowA(a, win_name) };
 
-        if hwnd.0 != 0 {
-            Some(Self { game_window: hwnd })
-        } else {
-            None
+        if game_window.0 != 0 {
+            return Some(Self { game_window });
         }
+        None
     }
 
-    pub fn update_window_sizepos(&self, display: &Display) {
+    pub fn update_window_pos(&self, display: &Display) {
         let win_rect = &mut [0u8; size_of::<RECT>()];
 
         unsafe {
@@ -44,12 +45,24 @@ impl WindowManager {
         }
         let win_rect: RECT = unsafe { transmute(*win_rect) };
 
-        let gl_window = display.gl_window();
-        let window = gl_window.window();
-        window.set_outer_position(PhysicalPosition::new(win_rect.left, win_rect.top));
+        let x_offset = unsafe {
+            GetSystemMetrics(SYSTEM_METRICS_INDEX(6))
+        };
 
-        let width = win_rect.right - win_rect.left;
-        let height = win_rect.bottom - win_rect.top;
-        window.set_inner_size(PhysicalSize::new(width, height));
+        let y_offset = unsafe {
+            GetSystemMetrics(SYSTEM_METRICS_INDEX(33))
+            + GetSystemMetrics(SYSTEM_METRICS_INDEX(92))
+            + GetSystemMetrics(SYSTEM_METRICS_INDEX(4))
+        };
+
+        let gl_win = display.gl_window();
+        let display_win = gl_win.window();
+        display_win.set_outer_position(PhysicalPosition::new(win_rect.left + x_offset, win_rect.top + y_offset));
+    }
+
+    pub fn update_window_size(&self, display: &Display, rect: Rect) {
+        let gl_win = display.gl_window();
+        let display_win = gl_win.window();
+        display_win.set_inner_size(PhysicalSize::new(rect.width(), rect.height()));
     }
 }
