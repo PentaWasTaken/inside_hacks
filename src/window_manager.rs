f06525a9da0d8983473a28a56ca264a0c28b0358
Piet Helzel <piet.helzel@outlook.com>
2021-11-09 23:43:53 +0100

    Added window visibility handling

--- src/window_manager.rs
+++ src/window_manager.rs
@@ -1,4 +1,5 @@
 use glium::Display;
+use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
 
 use std::ffi::CString;
 use std::mem::{size_of, transmute};
@@ -6,7 +7,11 @@ use std::ptr;
 
 use windows::Win32::Foundation::{HWND, PSTR, RECT};
 use windows::Win32::Graphics::Dwm::{DwmGetWindowAttribute, DWMWINDOWATTRIBUTE};
-use windows::Win32::UI::WindowsAndMessaging::{FindWindowA, GetSystemMetrics, SYSTEM_METRICS_INDEX};
+use windows::Win32::UI::Input::KeyboardAndMouse::EnableWindow;
+use windows::Win32::UI::WindowsAndMessaging::{
+    FindWindowA, GetForegroundWindow, GetSystemMetrics, SetForegroundWindow, SetWindowLongA,
+    ShowWindow, SHOW_WINDOW_CMD, SYSTEM_METRICS_INDEX, WINDOW_LONG_PTR_INDEX,
+};
 
 use glutin::dpi::{PhysicalPosition, PhysicalSize};
 
@@ -14,10 +19,12 @@ use egui::Rect;
 
 pub struct WindowManager {
     game_window: HWND,
+    overlay_window: HWND,
+    win_offset: (i32, i32),
 }
 
 impl WindowManager {
-    pub fn new(win_name: &str) -> Option<Self> {
+    pub fn new(win_name: &str, display: &Display) -> Option<Self> {
         let win_ptr = CString::new(win_name).unwrap();
         let win_name = PSTR(win_ptr.as_ptr() as *mut u8);
 
@@ -25,8 +32,31 @@ impl WindowManager {
 
         let game_window = unsafe { FindWindowA(a, win_name) };
 
-        if game_window.0 != 0 {
-            return Some(Self { game_window });
+        let overlay_window = display.gl_window().window().raw_window_handle();
+
+        if let RawWindowHandle::Windows(overlay_window) = overlay_window {
+            let overlay_window = HWND(overlay_window.hwnd as _);
+            unsafe {
+                SetWindowLongA(overlay_window, WINDOW_LONG_PTR_INDEX(-20), 0x80);
+            }
+
+            let x_offset = unsafe { GetSystemMetrics(SYSTEM_METRICS_INDEX(6)) };
+
+            let y_offset = unsafe {
+                GetSystemMetrics(SYSTEM_METRICS_INDEX(33))
+                    + GetSystemMetrics(SYSTEM_METRICS_INDEX(92))
+                    + GetSystemMetrics(SYSTEM_METRICS_INDEX(4))
+            };
+            if game_window.0 != 0 {
+                unsafe {
+                    SetForegroundWindow(game_window);
+                }
+                return Some(Self {
+                    game_window,
+                    overlay_window,
+                    win_offset: (x_offset, y_offset),
+                });
+            }
         }
         None
     }
@@ -45,24 +75,32 @@ impl WindowManager {
         }
         let win_rect: RECT = unsafe { transmute(*win_rect) };
 
-        let x_offset = unsafe {
-            GetSystemMetrics(SYSTEM_METRICS_INDEX(6))
-        };
-
-        let y_offset = unsafe {
-            GetSystemMetrics(SYSTEM_METRICS_INDEX(33))
-            + GetSystemMetrics(SYSTEM_METRICS_INDEX(92))
-            + GetSystemMetrics(SYSTEM_METRICS_INDEX(4))
-        };
-
         let gl_win = display.gl_window();
         let display_win = gl_win.window();
-        display_win.set_outer_position(PhysicalPosition::new(win_rect.left + x_offset, win_rect.top + y_offset));
+        display_win.set_outer_position(PhysicalPosition::new(
+            win_rect.left + self.win_offset.0,
+            win_rect.top + self.win_offset.1,
+        ));
     }
 
     pub fn update_window_size(&self, display: &Display, rect: Rect) {
         let gl_win = display.gl_window();
         let display_win = gl_win.window();
         display_win.set_inner_size(PhysicalSize::new(rect.width(), rect.height()));
+    }
+
+    pub fn update_window_visibility(&self) {
+        let foreground_window = unsafe { GetForegroundWindow() };
+        if foreground_window == self.overlay_window || foreground_window == self.game_window {
+            unsafe {
+                EnableWindow(self.overlay_window, true);
+                ShowWindow(self.overlay_window, SHOW_WINDOW_CMD(5));
+            }
+        } else {
+            unsafe {
+                EnableWindow(self.overlay_window, false);
+                ShowWindow(self.overlay_window, SHOW_WINDOW_CMD(0));
+            }
+        }
     }
 }
