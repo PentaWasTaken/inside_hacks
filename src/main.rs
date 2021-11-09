0a5828afaff1d4eaf5dcecd1a3a7ae74976c2abc
Piet Helzel <piet.helzel@outlook.com>
2021-11-09 23:52:18 +0100

    Hides the console on startup

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ mod window_manager;
 
 use glium::glutin::event_loop::EventLoop;
 
+use windows::Win32::System::Console::GetConsoleWindow;
+use windows::Win32::UI::WindowsAndMessaging::{ShowWindow, SHOW_WINDOW_CMD};
+
 fn main() {
+    //Hide console
+    unsafe {
+        let console_window = GetConsoleWindow();
+        ShowWindow(console_window, SHOW_WINDOW_CMD(0));
+    }
     let event_loop = EventLoop::with_user_event();
     let interface = Interface::new(&event_loop);
 
