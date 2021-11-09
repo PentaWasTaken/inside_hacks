55c171ceadba5501594656d42d29860428a35c4a
Piet Helzel <piet.helzel@outlook.com>
2021-11-09 23:42:50 +0100

    Refactor, changed struct layout

--- src/interface.rs
+++ src/interface.rs
@@ -5,15 +5,16 @@ use glutin::event_loop::{ControlFlow, EventLoop};
 
 use crate::window_manager::WindowManager;
 
-use egui::{Layout, Align};
+use egui::{Align, Layout};
 
 pub struct Interface {
     display: Display,
-    event_loop: EventLoop<()>,
+    egui_glium: EguiGlium,
+    window_manager: WindowManager,
 }
 
 impl Interface {
-    pub fn new(event_loop: EventLoop<()>) -> Self {
+    pub fn new(event_loop: &EventLoop<()>) -> Self {
         let window_builder = glutin::window::WindowBuilder::new()
             .with_resizable(false)
             .with_transparent(true)
@@ -27,15 +28,18 @@ impl Interface {
             .with_vsync(true);
 
         let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
+        let egui_glium = EguiGlium::new(&display);
+        let window_manager = WindowManager::new("INSIDE", &display).unwrap();
 
         Self {
             display,
-            event_loop,
+            egui_glium,
+            window_manager,
         }
     }
 
-    fn redraw(display: &Display, egui_glium: &mut EguiGlium, control_flow: &mut ControlFlow, window_manager: &WindowManager) {
-        egui_glium.begin_frame(display);
+    fn redraw(&mut self, control_flow: &mut ControlFlow) {
+        self.egui_glium.begin_frame(&self.display);
 
         let frame = egui::containers::Frame {
             margin: egui::vec2(5.0, 5.0),
@@ -48,62 +52,62 @@ impl Interface {
             stroke: egui::Stroke::new(2.0, egui::Color32::BLACK),
         };
 
+        let mut open = true;
         egui::Window::new("test")
             .frame(frame)
             .resizable(false)
             .fixed_pos((0.0, 0.0))
             .fixed_size((100.0, 50.0))
-            .show(egui_glium.ctx(), |ui| {
+            .open(&mut open)
+            .show(self.egui_glium.ctx(), |ui| {
                 ui.with_layout(Layout::top_down_justified(Align::Center), |ui| {
                     if ui.button("abc").clicked() {
                         println!("Yo");
                     };
                 });
-                window_manager.update_window_size(&display, ui.ctx().used_rect());
+                self.window_manager
+                    .update_window_size(&self.display, ui.ctx().used_rect());
             });
-            
-        let (needs_repaint, shapes) = egui_glium.end_frame(display);
 
-        if needs_repaint {
-            display.gl_window().window().request_redraw();
+        let (needs_repaint, shapes) = self.egui_glium.end_frame(&self.display);
+
+        if !open {
+            *control_flow = ControlFlow::Exit;
+        } else if needs_repaint {
+            self.display.gl_window().window().request_redraw();
             *control_flow = ControlFlow::Poll;
         } else {
             *control_flow = ControlFlow::Wait;
         }
 
-        let mut target = display.draw();
+        let mut target = self.display.draw();
 
         let color = egui::Rgba::TRANSPARENT;
         target.clear_color_srgb(color[0], color[1], color[2], color[3]);
 
-        egui_glium.paint(&display, &mut target, shapes);
+        self.egui_glium.paint(&self.display, &mut target, shapes);
 
         target.finish().unwrap();
     }
 
-    pub fn run(self) {
-        let mut egui_glium = EguiGlium::new(&self.display);
-
-        let window_manager = WindowManager::new("INSIDE").unwrap();
-
-        self.event_loop.run(move |event, _, mut control_flow| {
-            match event {
-                Event::RedrawEventsCleared => {
-                    window_manager.update_window_pos(&self.display);
-                    Interface::redraw(&self.display, &mut egui_glium, &mut control_flow, &window_manager);
-                }
-
-                Event::WindowEvent { event, .. } => {
-                    if egui_glium.is_quit_event(&event) {
-                        *control_flow = ControlFlow::Exit;
-                    }
-
-                    egui_glium.on_event(&event);
-
-                    self.display.gl_window().window().request_redraw();
-                }
-                _ => (),
+    pub fn run(mut self, event_loop: EventLoop<()>) {
+        event_loop.run(move |event, _, mut control_flow| match event {
+            Event::RedrawEventsCleared => {
+                self.window_manager.update_window_pos(&self.display);
+                self.window_manager.update_window_visibility();
+                self.redraw(&mut control_flow)
             }
+
+            Event::WindowEvent { event, .. } => {
+                if self.egui_glium.is_quit_event(&event) {
+                    *control_flow = ControlFlow::Exit;
+                }
+
+                self.egui_glium.on_event(&event);
+
+                self.display.gl_window().window().request_redraw();
+            }
+            _ => (),
         });
     }
 }
