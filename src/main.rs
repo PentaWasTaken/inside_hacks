daa276fe29cd3824a287698a75091f62c6c26d02
Piet Helzel <piet.helzel@outlook.com>
2021-11-11 21:25:16 +0100

    Initializes widgets before starting

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use interface::Interface;
 mod window_manager;
 
 mod widgets;
+use widgets::Widgets;
 
 use glium::glutin::event_loop::EventLoop;
 
@@ -11,13 +12,16 @@ use windows::Win32::System::Console::GetConsoleWindow;
 use windows::Win32::UI::WindowsAndMessaging::{ShowWindow, SHOW_WINDOW_CMD};
 
 fn main() {
+    println!("Loading...");
+    let widgets = Widgets::new();
+
     //Hide console
     unsafe {
         let console_window = GetConsoleWindow();
         ShowWindow(console_window, SHOW_WINDOW_CMD(0));
     }
     let event_loop = EventLoop::with_user_event();
-    let interface = Interface::new(&event_loop);
+    let interface = Interface::new(&event_loop, widgets);
 
     interface.run(event_loop);
 }
