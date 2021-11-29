4a09c8083fac1b786d201a6c0ab8a2f9a694f9b2
Piet Helzel <piet.helzel@outlook.com>
2021-11-29 23:00:32 +0100

    Added new widgets

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -25,27 +25,27 @@ impl Widgets {
 
         let inside_base = memhook.get_module_base_address("INSIDE.exe").unwrap();
 
-        let pos_sizes = (90.0, 60.0, 100.0);
+        let sizes = (90.0, 60.0, 100.0);
 
         let x_pos_edit = Box::new(ValueEditor::<f32>::new(
             "X-Position".to_string(),
             inside_base,
             vec![0xF92610, 0x4c0, 0x10, 0x98, 0x670, 0x0, 0x58, 0x70, 0x10],
-            pos_sizes,
+            sizes,
         ));
 
         let y_pos_edit = Box::new(ValueEditor::<f32>::new(
             "Y-Position".to_string(),
             inside_base,
             vec![0xF92610, 0x4c0, 0x10, 0x98, 0x670, 0x0, 0x58, 0x70, 0x14],
-            pos_sizes,
+            sizes,
         ));
 
         let z_pos_edit = Box::new(ValueEditor::<f32>::new(
             "Z-Position".to_string(),
             inside_base,
             vec![0x1001FA0, 0x260, 0x2E8, 0x318, 0x10, 0x28, 0x40, 0x18],
-            pos_sizes,
+            sizes,
         ));
 
         let jump_strength_edit = Box::new(ValueEditor::<f32>::new(
@@ -54,7 +54,7 @@ impl Widgets {
             vec![
                 0xF99BA8, 0xC8, 0x78, 0x188, 0x28, 0xD8, 0x20, 0x68, 0x30, 0x18, 0x14,
             ],
-            pos_sizes,
+            sizes,
         ));
 
         let run_speed_edit = Box::new(ValueEditor::<f32>::new(
@@ -63,7 +63,7 @@ impl Widgets {
             vec![
                 0xF99BA8, 0xC8, 0x78, 0x188, 0x28, 0xD8, 0x20, 0x68, 0x28, 0x14,
             ],
-            pos_sizes,
+            sizes,
         ));
 
         let swim_speed_edit = Box::new(ValueEditorMultiple::<f32, 3>::new(
@@ -80,14 +80,21 @@ impl Widgets {
                     0xF99BA8, 0xC8, 0x78, 0x188, 0x28, 0xD8, 0x20, 0x68, 0x68, 0x20,
                 ],
             ],
-            pos_sizes,
+            sizes,
+        ));
+
+        let sub_speed_edit = Box::new(ValueEditor::<f32>::new(
+            "Sub Speed".to_string(),
+            inside_base,
+            vec![0xF8D708, 0x118, 0xC0, 0x868, 0x158, 0x20, 0x100, 0x1EC],
+            sizes,
         ));
 
         let breath_edit = Box::new(ValueEditor::<f32>::new(
             "Breath".to_string(),
             inside_base,
             vec![0xF92610, 0x18, 0xE0, 0x98, 0x508, 0x20, 0x28, 0x104],
-            pos_sizes,
+            sizes,
         ));
 
         let save_state = Box::new(Savestate::new(
@@ -104,7 +111,24 @@ impl Widgets {
             inside_base,
             vec![0xF8D5D0, 0x38, 0xDC8, 0x28, 0x0, 0x130, 0xB4, 0xC2C],
             vec![0xFF, 0x90, 0xC0, 0x00, 0x00, 0x00],
-            vec![0x90, 0x90, 0x90, 0x90, 0x90, 0x90],
+            vec![0x90; 6],
+        ));
+
+        let instant_boost_hack = Box::new(Checkbox::new(
+            "Submarine Instant Boost".to_string(),
+            inside_base,
+            vec![0xF8D708, 0x118, 0xC0, 0x868, 0x158, 0x20, 0x100, 0x70, 0xB4],
+            vec![0x0, 0x0, 0x0, 0x3F, 0x0, 0x0, 0x80, 0x40],
+            vec![0x0; 8],
+        ));
+
+        let boy_size_edit = Box::new(ValueEditor::<f32>::new(
+            "Boy Size".to_string(),
+            inside_base,
+            vec![
+                0xF99BA8, 0xC8, 0x78, 0x188, 0x28, 0xD8, 0x20, 0x48, 0x20, 0x90,
+            ],
+            sizes,
         ));
 
         Self {
@@ -115,9 +139,12 @@ impl Widgets {
                 jump_strength_edit,
                 run_speed_edit,
                 swim_speed_edit,
+                sub_speed_edit,
                 breath_edit,
+                boy_size_edit,
                 save_state,
                 fall_damage_hack,
+                instant_boost_hack,
             ],
             memhook,
         }
